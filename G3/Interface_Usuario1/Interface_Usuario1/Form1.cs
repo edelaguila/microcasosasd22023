148d7c020f41cb0db61107a888fc2c4af18aeb07
D4nnny <dperezf5@miumg.edu.gt>
2023-07-27 16:07:30 -0600

    Agregando componentes al Fom de usuario

--- G3/Interface_Usuario1/Interface_Usuario1/Form1.cs
+++ G3/Interface_Usuario1/Interface_Usuario1/Form1.cs
@@ -16,5 +16,10 @@ namespace Interface_Usuario1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
