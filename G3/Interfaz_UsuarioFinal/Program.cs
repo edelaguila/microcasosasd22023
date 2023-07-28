13e045504dee3118bb4b52e89d96b6642e4491e4
D4nnny <dperezf5@miumg.edu.gt>
2023-07-28 17:26:35 -0600

    Actualizando Interfaz de Usuario

--- G3/Interfaz_UsuarioFinal/Program.cs
+++ G3/Interfaz_UsuarioFinal/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Interface_Usuario1
+namespace Interfaz_UsuarioFinal
 {
     static class Program
     {
--- G3/Interfaz_UsuarioFinal/Properties/Settings.Designer.cs
+++ G3/Interfaz_UsuarioFinal/Properties/Settings.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace Interface_Usuario1.Properties
+namespace Interfaz_UsuarioFinal.Properties
 {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
